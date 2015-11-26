5de954a2e4f913eb0cd32fb44939800ebca21823
drkdagron <ste.mckillop@gmail.com>
2015-11-26 09:06:04 -0500

    First Commit & Current Build

---
+++ WKSWeb/src/wks/gamelogic/AIMove.java
@@ -0,0 +1,16 @@
+package wks.gamelogic;
+
+public class AIMove {
+
+	public int X;
+	public int Y;
+	
+	public AIMove() {
+	}
+	public AIMove(int x, int y)
+	{
+		this.X = x;
+		this.Y = y;
+	}
+
+}
